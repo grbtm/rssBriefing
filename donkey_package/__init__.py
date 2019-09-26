3546c0bf6fee209efabfcb63830408899feeb393
GT <52323259+grbtm@users.noreply.github.com>
2019-09-26 13:43:23 +0200

    setting up database connection, sql schema and donkey_package __init__.py according to flask example tut

---
+++ donkey_package/__init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the WSGI application
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # use route() decorator to tell Flask what URL should trigger our function
+    # URL --> function
+    # to go function --> URL, use url_for(function_string)
+    # By default, a route only answers to GET requests
+    @app.route('/')
+    # The function is given a name which is also used to generate URLs for that particular function,
+    # and returns the message we want to display in the user’s browser
+    def hello_world():
+        # The return value from a view function is automatically converted into a response object
+        # for you.
+        return 'Hello wurd!'
+
+    from . import db
+    db.init_app(app)
+
+    return app
---
+++ donkey_package/db.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    # g is a special object that is unique for each request.
+    # It is used to store data that might be accessed by multiple functions during the request
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        # sqlite3.Row tells the connection to return rows that behave like dicts.
+        # This allows accessing the columns by name.
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db():
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
