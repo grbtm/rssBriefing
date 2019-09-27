558ed490d4c0736fefd720204659ebbe677a9730
GT <52323259+grbtm@users.noreply.github.com>
2019-09-27 21:06:00 +0200

    adding rss_reader blueprint functionalities

--- donkey_package/rss_reader.py
+++ donkey_package/rss_reader.py
@@ -5,10 +5,17 @@ from werkzeug.exceptions import abort
 from donkey_package.auth import login_required
 from donkey_package.db import get_db
 
-
 bp = Blueprint('rss_reader', __name__)
+
 
 @bp.route('/')
 def index():
     db = get_db()
-
+    items = db.execute(
+        'SELECT i.id, i.title, i.description, created, f.title as feed_title'
+        ' FROM item i JOIN feed f ON i.feed_id = f.id'
+        ' JOIN user_feed uf on uf.feed_id = f.id'
+        ' JOIN user u on u.id = uf.user_id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('rss_reader/index.html', items=items)
