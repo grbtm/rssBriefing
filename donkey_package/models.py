0bde6afe5804ffeedb3bc49f166617783fb97c13
GT <52323259+grbtm@users.noreply.github.com>
2019-12-10 18:51:23 +0100

    finished declaration of sqlalchemy models

--- donkey_package/models.py
+++ donkey_package/models.py
@@ -13,8 +13,8 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    feeds = db.relationship('Feed', secondary=user_feed, lazy='subquery',
-                            backref=db.backref('users', lazy=True))
+    feeds = db.relationship('Feed', secondary=user_feed, lazy='subquery', backref=db.backref('users', lazy=True))
+    briefing_items = db.relationship('Briefing', lazy=True, backref=db.backref('user', lazy='joined'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -26,6 +26,8 @@ class Feed(db.Model):
     description = db.Column(db.String())
     link = db.Column(db.String())
     href = db.Column(db.String())
+    items = db.relationship('Item', lazy=True, backref=db.backref('feed', lazy='joined'))
+    briefing_items = db.relationship('Briefing', lazy=True, backref=db.backref('feed', lazy='joined'))
 
     def __repr__(self):
         return '<Feed {}>'.format(self.title)
@@ -33,9 +35,30 @@ class Feed(db.Model):
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(), index=True)
+    title = db.Column(db.String(), index=True, nullable=False)
     description = db.Column(db.String())
-    link = db.Column(db.String())
+    link = db.Column(db.String(), nullable=False)
     created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     guid = db.Column(db.String())
     feed_id = db.Column(db.Integer, db.ForeignKey('feed.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Feed {}, Item {}>'.format(self.feed_id, self.title)
+
+
+class Briefing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), index=True, nullable=False)
+    description = db.Column(db.String())
+    link = db.Column(db.String(), nullable=False)
+    reference = db.Column(db.String())
+    score = db.Column(db.Float())
+    created = db.Column(db.DateTime)
+    briefing_created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    guid = db.Column(db.String())
+
+    feed_title = db.Column(db.Integer, db.ForeignKey('feed.title'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Briefing item title {}, feed {}, user {}>'.format(self.title, self.feed_id, self.user_id)
