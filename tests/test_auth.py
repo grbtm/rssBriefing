f6126dc2067f9a5f06cae453b588e65ba33c69be
GT <52323259+grbtm@users.noreply.github.com>
2020-01-08 15:44:19 +0100

    added auth login test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,17 +6,12 @@ from donkey_package.db_utils import get_user_by_username
 
 
 def test_register(client, app):
-
     assert client.get('/auth/register').status_code == 200
-
     response = client.post(
         '/auth/register', data={'username': 'a', 'email': 'a@a.com', 'password': 'a'}
     )
-
     assert 'http://localhost/auth/login' == response.headers['Location']
-
     with app.app_context():
-
         assert get_user_by_username('a') is not None
 
 
@@ -32,3 +27,14 @@ def test_register_validate_input(client, username, email, password, message):
         data={'username': username, 'email': email, 'password': password}
     )
     assert message in response.data
+
+
+def test_login(client, auth):
+    assert client.get('/auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers['Location'] == 'http://localhost/'
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user.username == 'test'
