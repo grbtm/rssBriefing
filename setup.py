f05dd098440b0d402d5490cb246e5612c302be59
GT <52323259+grbtm@users.noreply.github.com>
2020-01-09 16:31:59 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='donkeyBriefing',
+    name='rssBriefing',
     version='0.1',
     author="Tom",
     author_email="grbtm@posteo.net",
     description="A RSS/Atom fueled daily briefing app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/grbtm/project_donkey",
+    url="https://github.com/grbtm/rssBriefing",
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
