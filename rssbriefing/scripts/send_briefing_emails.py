5e637e0a9c1b3854bfe508f6c06b02eb028d37c6
GT <52323259+grbtm@users.noreply.github.com>
2020-06-10 16:03:44 +0200

    update email template and add text wrapping for email text

--- rssbriefing/scripts/send_briefing_emails.py
+++ rssbriefing/scripts/send_briefing_emails.py
@@ -1,12 +1,13 @@
 import argparse
 import sys
+from textwrap import TextWrapper
 
 from flask import render_template
 
 from rssbriefing import create_app
 from rssbriefing.briefing_utils import get_standard_briefing
-from rssbriefing.email import send_single_mail
 from rssbriefing.db_utils import get_user_by_id, get_all_users
+from rssbriefing.email import send_single_mail
 
 
 def parse_args():
@@ -48,7 +49,13 @@ def main():
 
         app.logger.info(
             f'Latest briefing was generated on {latest_briefing_date} and contains {len(briefing_items)} items.')
+
         if briefing_items:
+            # Wrap each summary into a paragraph of max width 70 chars
+            wrapper = TextWrapper()
+            for item in briefing_items:
+                item.summary = wrapper.fill(item.summary)
+
             for user in users:
                 app.logger.info(f'Sending briefing for user {user}...')
                 send_single_mail(subject="Today's RoboBriefing",
@@ -57,10 +64,6 @@ def main():
                                  text_body=render_template('briefing/briefing_email.txt',
                                                            user=user, items=briefing_items,
                                                            briefing_date=latest_briefing_date))
-
-
-
-
     except:
         app.logger.error('Unhandled exception', exc_info=sys.exc_info())
 
